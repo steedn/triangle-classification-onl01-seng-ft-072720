15c2f996c26cac2ec7dce25be2bc1df0e8f7bff4
Nathan Steed <steedn@outlook.com>
2020-08-17 08:37:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   attr_accessor :kind
   def initialize(side1, side2, side3)
-    if side1 == side2 == side3
+    if side1 == side2 && side2 == side3
       @kind = "equilateral"
     end
   end
