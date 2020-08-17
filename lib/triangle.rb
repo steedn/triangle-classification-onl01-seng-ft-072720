1178e64a7bcb4b58938b335b5c28b330d2f87ad5
Nathan Steed <steedn@outlook.com>
2020-08-17 08:56:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,10 +3,10 @@ class Triangle
   def initialize(side1, side2, side3)
     if side1 <= 0 || side2 <= 0 || side3 <= 0
       raise TriangleError
+    elsif  side1 == side2 && side2 == side3
+      @kind = :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
-      @kind = :isosceles
-    elsif side1 == side2 && side2 == side3
-       @kind = :equilateral
+       @kind = :isosceles
     else
       @kind = :scalene
     end
